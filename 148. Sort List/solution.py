2c5990ab20a8cfef213712e213d3dc9701fa5813
aurora <115104415+auroraliou@users.noreply.github.com>
2024-09-15 21:11:43 +0800

    Create solution.py

---
+++ 148. Sort List/solution.py
@@ -0,0 +1,48 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    
+        ## if no node or only one node
+        if not head or not head.next:
+            return head
+        
+        ## split the list into two halfs
+        left = head
+        right = self.getMid(head)
+        tmp = right.next
+        right.next = None
+        right = tmp
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+        return self.merge(left, right)
+
+    def getMid(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            ## fast is 2 times faster than slow
+            ## So when fast reaches end, slow is in the mid
+        return slow
+    def merge(self, left, right):
+        tail = dummy = ListNode()
+        while left and right:
+            if left.val < right.val:
+                tail.next = left
+                left = left.next
+            else:
+                tail.next = right
+                right = right.next
+            tail = tail.next
+        if left:
+            tail.next = left
+        if right:
+            tail.next = right
+        return dummy.next
+        
+        
