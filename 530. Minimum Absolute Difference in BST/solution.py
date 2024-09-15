01aea056021ec03e174fb672a82f2cf6de6654ca
aurora <115104415+auroraliou@users.noreply.github.com>
2024-09-15 21:10:05 +0800

    Create solution.py

---
+++ 530. Minimum Absolute Difference in BST/solution.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        # if not root:
+        #     return None
+        # q = deque([root])
+        # minDiff = float('INF')
+        
+        # for i in range(len(q)):
+        prev, res = None, float("INF")
+
+
+        def dfs(node) -> int:
+            if not node:
+                return
+            dfs(node.left)
+            nonlocal prev, res
+
+            if prev:
+                res = min(res, node.val - prev.val)
+            prev = node
+            dfs(node.right)
+        dfs(root)
+        return res
